f3f13a6cb477e1efa28822db7d2a6f1b3cad23ee
MarielySouza <mariely29042009@gmail.com>
2026-08-04 21:30:31 -0300

    Add files via upload

--- trabalho1LP3/Data.php
+++ trabalho1LP3/Data.php
@@ -1,1 +1,33 @@
+<?php   
 
+class Data{
+
+        private int $dia;
+        private int $mes;
+        private int $ano;
+
+    public function __construct(int $dia, int $mes, int $ano)
+    {
+        $this->dia = $dia;
+        $this->mes = $mes;
+        $this->ano = $ano;
+    }
+
+        public function __toString() 
+        {
+         return "$this->dia/$this->mes/$this->ano";
+        }
+
+        public function imprimirDataBanco()
+        {
+            //Gente Deus me ajude como faz essa parte???
+            echo ('d')/('m')/('y')
+
+        }
+    
+}
+
+
+
+
+?>
