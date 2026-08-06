1862de964d012bbb6bcac62d44458d0d833b39a2
Skayzinxx1 <matheusgeo50@gmail.com>
2026-08-06 08:35:21 -0300

    Add files via upload

--- trabalho1LP3/ItemPedido.php
+++ trabalho1LP3/ItemPedido.php
@@ -1,1 +1,77 @@
+<?php 
+require_once "Produto.php";
 
+class ItemPedido{
+    private Produto $item;
+    private int $quantidade;
+    private float $valor;
+
+    public function __construct(Produto $item, int $quantidade, float $valor)
+    {
+        $this->item=$item;
+        $this->quantidade=$quantidade;
+        $this->valor=$valor;
+        
+    }
+
+    /**
+     * Get the value of item
+     */ 
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    /**
+     * Set the value of item
+     *
+     * @return  self
+     */ 
+    public function setItem(Produto $item)
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of quantidade
+     */ 
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    /**
+     * Set the value of quantidade
+     *
+     * @return  self
+     */ 
+    public function setQuantidade(int $quantidade)
+    {
+        $this->quantidade = $quantidade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of valor
+     */ 
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    /**
+     * Set the value of valor
+     *
+     * @return  self
+     */ 
+    public function setValor(float $valor)
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+}
+?>
